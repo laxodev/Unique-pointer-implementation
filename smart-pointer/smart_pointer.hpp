262411d3614ff94f3c5bc9713580d597f02956ee
laxodev <50239792+laxodev@users.noreply.github.com>
2019-05-07 16:38:50 +0930

    Fixed issue with swap member not taking in a deleter in the template

--- smart-pointer/smart_pointer.hpp
+++ smart-pointer/smart_pointer.hpp
@@ -60,7 +60,7 @@ public:
 		return ptr_resource;
 	}
 	// swaps the resources
-	void swap(unique_ptr<T>& resource_ptr) noexcept
+	void swap(unique_ptr<T, deleter>& resource_ptr) noexcept
 	{
 		std::swap(ptr_resource, resource_ptr.ptr_resource);
 	}
